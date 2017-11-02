f14057062cd049b2363be28f12fd8e85583a72c9
keerthi varumbudy <keerthi.vrmd@gmail.com>
2017-11-02 20:04:01 +0530

    Complaints menu and accept complain pages made. Database yet to be added

---
+++ app/src/main/java/com/example/kushagra/first/ComplaintsMenu.java
@@ -0,0 +1,26 @@
+package com.example.kushagra.first;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class ComplaintsMenu extends Activity{
+    private Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.complaints_menu);
+        button1= (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+    public void onClick(View v){
+        Intent intent1 = new Intent(ComplaintsMenu.this, AcceptComplaint.class);
+
+
+        ComplaintsMenu.this.startActivity(intent1);
+    }});
+
+    }}
