dd7a686ac2be357e272706584069f21d066553fa
keerthi varumbudy <keerthi.vrmd@gmail.com>
2017-11-16 14:09:45 +0530

    Database successfully created.

---
+++ app/src/main/java/com/example/kushagra/first/ComplaintInformation.java
@@ -0,0 +1,28 @@
+package com.example.kushagra.first;
+
+/**
+ * Created by Keerthi on 09-11-2017.
+ */
+
+public class ComplaintInformation {
+   public String ComplaintID;
+    public  String ComplaintDetails;
+    public String ComplaintLocation;
+    public  String Complaint;
+
+    public ComplaintInformation(){
+
+    }
+
+
+
+
+    public ComplaintInformation(String complaintID, String complaintDetails, String complaintLocation, String complaint) {
+        ComplaintID = complaintID;
+        ComplaintDetails = complaintDetails;
+        ComplaintLocation = complaintLocation;
+        Complaint = complaint;
+    }
+
+}
+
