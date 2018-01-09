67bbd75f6d73a74639290adae15d954a40308d52
keerthi varumbudy <keerthi.vrmd@gmail.com>
2018-01-09 10:43:49 +0530

    Sign up page created. Functional. Login has bugs.

--- app/src/main/java/com/example/kushagra/first/Login.java
+++ app/src/main/java/com/example/kushagra/first/Login.java
@@ -1,12 +1,22 @@
 package com.example.kushagra.first;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -14,32 +24,69 @@ import com.google.firebase.database.FirebaseDatabase;
 public class Login extends AppCompatActivity {
     private Button LoginButton;
     private Button New;
+    private FirebaseAuth mAuth;
+    private EditText Email;
+    private EditText Password;
+    private ProgressBar ProgressBar;
+    String email;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        LoginButton= (Button) findViewById(R.id.Login);
-        New= (Button) findViewById(R.id.newSignUp);
+        LoginButton = (Button) findViewById(R.id.Login);
+        New = (Button) findViewById(R.id.newSignUp);
+        mAuth = FirebaseAuth.getInstance(); //Authentication getting instance
+        Email = (EditText) findViewById(R.id.Email);
+        Password = (EditText) findViewById(R.id.Password);
+        ProgressBar = (ProgressBar) findViewById(R.id.ProgressBar);
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
+              // login();
+                Intent intent1 = new Intent(Login.this, ComplaintsMenu.class);
 
-            LoginButton.setOnClickListener(new View.OnClickListener(){
-                public void onClick(View v){
-                    Intent intent1 = new Intent(Login.this, ComplaintsMenu.class);
-
-                    Login.this.startActivity(intent1);
-
-
-    }
-});
-        New.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+                Login.this.startActivity(intent1);
+            }
+        });
+        New.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 Intent intent1 = new Intent(Login.this, SignUp.class);
 
                 Login.this.startActivity(intent1);
 
 
             }
-        });
-            }
+        });}
+
+
+    private void login() {
+        email = Email.getText().toString().trim();
+        password = Password.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            Intent intent1 = new Intent(Login.this, ComplaintsMenu.class);
+
+                            Login.this.startActivity(intent1);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Login.this, "Login failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        // ...
+                    }
+
+                });
+
+
+    }
 }
+
--- app/src/main/java/com/example/kushagra/first/SignUp.java
+++ app/src/main/java/com/example/kushagra/first/SignUp.java
@@ -1,13 +1,123 @@
 package com.example.kushagra.first;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.Activity;
 
-public class SignUp extends AppCompatActivity {
+import android.support.annotation.NonNull;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUp extends Activity {
+
+    private Button Submit;
+    private EditText UserName;
+    private EditText Email;
+    private EditText Phone;
+    private EditText Password;
+    private EditText ConfirmPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        Submit = (Button) findViewById(R.id.Submit);
+        UserName = (EditText) findViewById(R.id.UserName);
+        Email = (EditText) findViewById(R.id.Email);
+        Phone = (EditText) findViewById(R.id.Phone);
+        Password = (EditText) findViewById(R.id.Password);
+        ConfirmPassword = (EditText) findViewById(R.id.ConfirmPassword);
+        mAuth = FirebaseAuth.getInstance();
+
+        Submit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                RegisterUser();
+            }
+        });}
+
+
+    private void RegisterUser() {
+        String userName = UserName.getText().toString().trim();
+        String email= Email.getText().toString().trim();
+        String phone= Phone.getText().toString().trim();
+        String password= Password.getText().toString().trim();
+        String confirmPassword= ConfirmPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(userName)) {
+            Toast.makeText(this, "Enter User Name", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Enter Email Address", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(this, "Enter valid Email Address", Toast.LENGTH_LONG).show();
+            return;
+
+        }
+        else if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(this, "Enter Phone Number", Toast.LENGTH_LONG).show();
+            return;
+        }
+        else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Enter Password", Toast.LENGTH_LONG).show();
+            return;
+        }
+        else if(password.length()<6){
+            Toast.makeText(this, "Password length should be more than 6 characters", Toast.LENGTH_LONG).show();
+            return;
+        }
+        else if (TextUtils.isEmpty(confirmPassword)) {
+            Toast.makeText(this, "Please Confirm Password", Toast.LENGTH_LONG).show();
+            return;
+        }
+        else {
+            if (!password.equals(confirmPassword)) {
+                Toast.makeText(this, "Please Confirm Password again", Toast.LENGTH_LONG).show();
+                return;
+            }
+            else {
+                //Create User with Email and Password
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                    //GO TO COMPLAINT PAGE
+
+                                    Toast.makeText(SignUp.this, "Sign up successful.", Toast.LENGTH_LONG).show();
+                                }
+                                    else {
+                                    if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                        Toast.makeText(SignUp.this, "You are already registered", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(SignUp.this, "Authentication failed.", Toast.LENGTH_LONG).show();
+                                        return;
+                                    }
+                                }
+
+                                // ...
+                            }
+                        });
+
+            }
+        }
     }
 }
