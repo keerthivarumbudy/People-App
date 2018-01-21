7435af0b7e287e1f1faaa7846cf0753b3a8a7a2f
Kushagra <vatsa.kushagra@gmail.com>
2018-01-21 23:20:28 +0530

    sign out done

--- app/src/main/java/com/example/kushagra/first/ComplaintsMenu.java
+++ app/src/main/java/com/example/kushagra/first/ComplaintsMenu.java
@@ -3,24 +3,85 @@ package com.example.kushagra.first;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class ComplaintsMenu extends Activity{
+
+public class ComplaintsMenu extends AppCompatActivity{
     private Button button1;
+    private Toolbar mToolbar;
+    private FirebaseAuth mAuth;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.complaints_menu);
         button1= (Button) findViewById(R.id.button1);
+        mAuth= FirebaseAuth.getInstance();
+
+
         button1.setOnClickListener(new View.OnClickListener(){
     public void onClick(View v){
         Intent intent1 = new Intent(ComplaintsMenu.this, complaints.class);
 
 
         ComplaintsMenu.this.startActivity(intent1);
+
+
+
     }});
 
-    }}
+    mToolbar=(Toolbar)findViewById(R.id.main_page_toolbar);
+    setSupportActionBar(mToolbar);
+    getSupportActionBar().setTitle("First");
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser= mAuth.getCurrentUser();
+        if(currentUser==null){
+            sendToStart();
+        }
+    }
+
+    private void sendToStart() {
+
+        Intent k= new Intent(ComplaintsMenu.this,Login.class);
+        startActivity(k);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId()==R.id.main_logout_button){
+            FirebaseAuth.getInstance().signOut();
+
+            sendToStart();
+        }
+
+        return true;
+    }
+}
