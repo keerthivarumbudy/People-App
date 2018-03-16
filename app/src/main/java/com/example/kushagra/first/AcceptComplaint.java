5aad9e636754058cbd4e18b278d1d7dcb890f2df
sarkar07 <saurovsarkar4@gmail.com>
2018-03-17 02:09:25 +0530

    update

--- app/src/main/java/com/example/kushagra/first/AcceptComplaint.java
+++ app/src/main/java/com/example/kushagra/first/AcceptComplaint.java
@@ -1,8 +1,10 @@
 package com.example.kushagra.first;
 
+import android.app.Activity;
+
 /**
  * Created by SAUROV SARKAR on 17-03-2018.
  */
 
-class AcceptComplaint {
+public class AcceptComplaint extends Activity {
 }
